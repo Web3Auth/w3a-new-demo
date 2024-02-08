13453394020214d25c7263ed443949303bf4a544
Chaitanya Potti <chaitanya.potti@gmail.com>
2024-02-08 15:00:47 +0800

    add gtag

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import './globals'
 import '@toruslabs/vue-components/style.css'
 import './assets/main.css'
 import { createIcons } from '@toruslabs/vue-components'
-
+import VueGtag from 'vue-gtag'
 import { createPinia } from 'pinia'
 
 import router from './routes'
@@ -13,6 +13,16 @@ import App from './App.vue'
 
 const app = createApp(App)
 
-app.use(router).use(createPinia()).use(createIcons(icons))
+app
+  .use(router)
+  .use(createPinia())
+  .use(createIcons(icons))
+  .use(
+    VueGtag,
+    {
+      config: { id: 'G-065WFNK5DW' }
+    },
+    router
+  )
 
 app.mount('#app')
