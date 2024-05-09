23d470e98803fb16693155660aee1ea369c8bca6
Chaitanya Potti <chaitanya.potti@gmail.com>
2024-05-09 12:29:34 +0800

    fix logo

--- src/store/web3authStore.ts
+++ src/store/web3authStore.ts
@@ -42,8 +42,8 @@ export const useWeb3authStore = defineStore('web3auth', () => {
       privateKeyProvider: privateKeyProvider,
       uiConfig: {
         uxMode: 'redirect',
-        logoDark: 'https://images.web3auth.io/web3auth-logo-w.svg',
-        logoLight: 'https://images.web3auth.io/web3auth-logo-w-light.svg',
+        logoLight: 'https://images.web3auth.io/web3auth-logo-w.svg',
+        logoDark: 'https://images.web3auth.io/web3auth-logo-w-light.svg',
         // logoDark: 'https://images.web3auth.io/login-farcaster-active.svg',
         // logoLight: 'https://images.web3auth.io/login-farcaster-light.svg',
         mode: 'light',
